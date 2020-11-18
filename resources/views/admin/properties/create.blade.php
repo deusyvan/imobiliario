26105abe5dc97d5f12b3c0c153f69d1cd95244a7
deusyvan-windows-prata <deusyvan@gmail.com>
2020-11-18 00:58:54 -0300

    Finalizando Fix Tinymce e Persistência

--- resources/views/admin/properties/create.blade.php
+++ resources/views/admin/properties/create.blade.php
@@ -19,8 +19,14 @@
     @include('admin.properties.filter')
 
     <div class="dash_content_app_box">
-
         <div class="nav">
+            @if($errors->all())
+                @foreach ($errors->all() as $error)
+                    @message(['color' => 'orange'])
+                    <p class="icon-asterisk">{{ $error }}</p>
+                    @endmessage
+                @endforeach
+            @endif
             <ul class="nav_tabs">
                 <li class="nav_tabs_item">
                     <a href="#data" class="nav_tabs_item_link active">Dados Cadastrais</a>
@@ -124,7 +130,7 @@
                         <div class="app_collapse">
                             <div class="app_collapse_header mt-2 collapse">
                                 <h3>Características</h3>
-                                <span class="icon-plus-circle icon-notext">condominium</span>
+                                <span class="icon-plus-circle icon-notext"></span>
                             </div>
 
                             <div class="app_collapse_content d-none">
@@ -195,7 +201,7 @@
 
                                 <label class="label">
                                     <span class="legend">Endereço:</span>
-                                    <input type="text" name="street" class="street" placeholder="Endereço Completo" value="{{ old('street') }}/>
+                                    <input type="text" name="street" class="street" placeholder="Endereço Completo" value="{{ old('street') }}" />
                                 </label>
 
                                 <div class="label_g2">
@@ -235,87 +241,87 @@
                         <div class="label_g5">
                             <div>
                                 <label class="label">
-                                    <input type="checkbox" name="air_conditioning"><span>Ar Condicionado</span>
+                                    <input type="checkbox" name="air_conditioning" {{ (old('air_conditioning') == 'on' || old('air_conditioning') == true ? 'checked' : '') }}><span>Ar Condicionado</span>
                                 </label>
                             </div>
                             <div>
                                 <label class="label">
-                                    <input type="checkbox" name="bar"><span>Bar</span>
+                                    <input type="checkbox" name="bar" {{ (old('bar') == 'on' || old('bar') == true ? 'checked' : '') }} ><span>Bar</span>
                                 </label>
                             </div>
                             <div>
                                 <label class="label">
-                                    <input type="checkbox" name="library"><span>Biblioteca</span>
-                                </label>
-                            </div>
-
-                            <div>
-                                <label class="label">
-                                    <input type="checkbox" name="barbecue_grill"><span>Churrasqueira</span>
+                                    <input type="checkbox" name="library" {{ (old('library') == 'on' || old('library') == true ? 'checked' : '') }}><span>Biblioteca</span>
                                 </label>
                             </div>
 
                             <div>
                                 <label class="label">
-                                    <input type="checkbox" name="american_kitchen"><span>Cozinha Americana</span>
+                                    <input type="checkbox" name="barbecue_grill" {{ (old('barbecue_grill') == 'on' || old('barbecue_grill') == true ? 'checked' : '') }}><span>Churrasqueira</span>
                                 </label>
                             </div>
 
                             <div>
                                 <label class="label">
-                                    <input type="checkbox" name="fitted_kitchen"><span>Cozinha Planejada</span>
-                                </label>
-                            </div>
-                            <div>
-                                <label class="label">
-                                    <input type="checkbox" name="pantry"><span>Despensa</span>
+                                    <input type="checkbox" name="american_kitchen" {{ (old('american_kitchen') == 'on' || old('american_kitchen') == true ? 'checked' : '') }}><span>Cozinha Americana</span>
                                 </label>
                             </div>
 
                             <div>
                                 <label class="label">
-                                    <input type="checkbox" name="edicule"><span>Edícula</span>
+                                    <input type="checkbox" name="fitted_kitchen" {{ (old('fitted_kitchen') == 'on' || old('fitted_kitchen') == true ? 'checked' : '') }}><span>Cozinha Planejada</span>
+                                </label>
+                            </div>
+                            <div>
+                                <label class="label">
+                                    <input type="checkbox" name="pantry" {{ (old('pantry') == 'on' || old('pantry') == true ? 'checked' : '') }}><span>Despensa</span>
                                 </label>
                             </div>
 
                             <div>
                                 <label class="label">
-                                    <input type="checkbox" name="office"><span>Escritório</span>
+                                    <input type="checkbox" name="edicule" {{ (old('edicule') == 'on' || old('edicule') == true ? 'checked' : '') }}><span>Edícula</span>
+                                </label>
+                            </div>
+
+                            <div>
+                                <label class="label">
+                                    <input type="checkbox" name="office" {{ (old('office') == 'on' || old('office') == true ? 'checked' : '') }}><span>Escritório</span>
                                 </label>
                             </div>
                             <div>
                                 <label class="label">
-                                    <input type="checkbox" name="bathtub"><span>Banheira</span>
+                                    <input type="checkbox" name="bathtub" {{ (old('bathtub') == 'on' || old('bathtub') == true ? 'checked' : '') }}><span>Banheira</span>
                                 </label>
                             </div>
                             <div>
                                 <label class="label">
-                                    <input type="checkbox" name="fireplace"><span>Lareira</span>
+                                    <input type="checkbox" name="fireplace" {{ (old('fireplace') == 'on' || old('fireplace') == true ? 'checked' : '') }}><span>Lareira</span>
                                 </label>
                             </div>
                             <div>
                                 <label class="label">
-                                    <input type="checkbox" name="lavatory"><span>Lavabo</span>
+                                    <input type="checkbox" name="lavatory" {{ (old('lavatory') == 'on' || old('lavatory') == true ? 'checked' : '') }}><span>Lavabo</span>
                                 </label>
                             </div>
                             <div>
                                 <label class="label">
-                                    <input type="checkbox" name="furnished"><span>Mobiliado</span>
+                                    <input type="checkbox" name="furnished" {{ (old('furnished') == 'on' || old('furnished') == true ? 'checked' : '') }}><span>Mobiliado</span>
                                 </label>
                             </div>
                             <div>
                                 <label class="label">
-                                    <input type="checkbox" name="pool"><span>Piscina</span>
+                                    <input type="checkbox" name="pool" {{ (old('pool') == 'on' || old('pool') == true ? 'checked' : '') }} ><span>Piscina</span>
                                 </label>
                             </div>
                             <div>
                                 <label class="label">
-                                    <input type="checkbox" name="steam_room"><span>Sauna</span>
+                                    <input type="checkbox" name="steam_room" {{ (old('steam_room') == 'on' || old('steam_room') == true ? 'checked' : '') }} ><span>Sauna</span>
                                 </label>
                             </div>
                             <div>
                                 <label class="label">
-                                    <input type="checkbox" name="view_of_the_sea"><span>Vista para o Mar</span>
+                                    <input type="checkbox" name="view_of_the_sea" {{ (old('view_of_the_sea') == 'on' || old('view_of_the_sea') == true ? 'checked' : '') }}><span>Vista para o Mar</span>
                                 </label>
                             </div>
                         </div>
